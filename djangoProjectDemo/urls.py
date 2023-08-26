a22daf498af75d76650ea82c0516db79985ccf89
Lionce <sakura.xue8@gmail.com>
2023-08-26 13:10:18 +0800

    New application—Dashboard

--- djangoProjectDemo/urls.py
+++ djangoProjectDemo/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
